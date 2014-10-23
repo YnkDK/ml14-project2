aa9bb05b212f4563b669f2a6a0f9f9e04f3289fd
Kasper Tvede <kasper@webmasteren.eu>
2014-10-23 13:18:14 +0200

    run and data.
    testing has code to make a generated file.

--- dimReduce.m
+++ dimReduce.m
@@ -1,18 +1,8 @@
 function newData = dimReduce( inValues, reduceTo )
 %DIMREDUCE reduces dimentionality
 %   Gandal's magical wand was here.
-
-% imshow(reshape(inValues(1,:), 28 , 28));  
-[coeff, score, variance] = princomp(inValues);
+[coeff, score, ~] = princomp(inValues);
 ndim = reduceTo; %new dim size ?
 newData = score(:,1:ndim)*coeff(1:ndim,1:ndim)';
-% should represent a 5
-% imshow(reshape(newData(1,:), sqrt(ndim) , sqrt(ndim)));  
-% imshow(reshape(newData(1,:), 24 , 25));  
-% imshow(reshape(newData(1,:), 20 ,30));  
-% imshow(reshape(newData(1,:), 25 , 24));  
-% imshow(reshape(newData(1,:), 24 , 25));  
-% imshow(reshape(newData(1,:), 15 , 40));  
-% imshow(reshape(newData(1,:), 40 , 15));  
 end
 
