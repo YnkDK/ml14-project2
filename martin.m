7bfd20d4aa81d6900af45f6fbe9b40727a0fdf46
YnkDK <ms@ynk.gl>
2014-10-28 14:44:03 +0100

    Permuting data

--- martin.m
+++ martin.m
@@ -10,19 +10,28 @@ trainDat    = load('auTrain.mat');
 data   = trainDat.au_train_digits;
 labels = trainDat.au_train_labels;
 
+% Generate more data
+[data,labels] = genData(data, labels, ceil(length(data)*1.3) );
+disp('done generating data');
+% Reduce dimensions
+data  = dimReduce(data,25*25);
+disp('done reducing data');
+% Scramble data
+idx = randperm(numel(labels))';
+
 % Split into a train and test set
 numTrain = round(3*numel(labels)/4);
  
-trainData   = data(1:numTrain,:);
-trainLabels = labels(1:numTrain)'; % Shift Labels to the Range 1-5
+trainData   = data(idx(1:numTrain),:);
+trainLabels = labels(idx(1:numTrain)); % Shift Labels to the Range 1-5
 
-testData   = data(numTrain+1:end,:);
-testLabels = labels(numTrain+1:end);   % Shift Labels to the Range 1-5
+testData   = data(idx(numTrain+1:end),:);
+testLabels = labels(idx(numTrain+1:end));   % Shift Labels to the Range 1-5
 disp('Ready to train...');
 
 % Train
 tic;
-model = bestRBF(data, labels);
+model = bestRBF(trainData, trainLabels);
 toc;
 % Test
 [predicted_label, accuracy, dp] = svmpredict(testLabels, testData, model);
@@ -31,22 +40,24 @@ toc;
 fprintf('Test Accuracy: %f%%\n', accuracy);
 
 % Print the misclassified
-diff = find((predicted_label - testLabels) ~= 0);
-misDat = testData(diff, :);
-misLab = testLabels(diff);
-preLab = predicted_label(diff);
+%diff = find((predicted_label - testLabels) ~= 0);
+%misDat = testData(diff, :);
+%misLab = testLabels(diff);
+%preLab = predicted_label(diff);
 
-imgs = cell(16,1);
-for i=1:16
-    imgs{i} = reshape(misDat(i, :), 28, 28);
-end
+%numShow = min(16, numel(misLab));
 
-subsize = ceil(sqrt(length(misLab)));
+%imgs = cell(16,1);
+%for i=1:16
+%    imgs{i} = reshape(misDat(i, :), 28, 28);
+%end
+
+%subsize = ceil(sqrt(length(misLab)));
 
 %# show them in subplots
-figure(1)
-for i=1:16
-    subplot(4,4,i);
-    h = imshow(imgs{i}, 'InitialMag',100, 'Border','tight');
-    title(sprintf('Lab.: %d Pre.: %d', misLab(i), preLab(i)))
-end
+%figure(1)
+%for i=1:16
+%    subplot(4,4,i);
+%    h = imshow(imgs{i}, 'InitialMag',100, 'Border','tight');
+%    title(sprintf('Lab.: %d Pre.: %d', misLab(i), preLab(i)))
+%end
